7b6f3b5bf7ec1e50fb8f43b04370d92e741a3ac6
Pratik kumar <61779448+Pratik038@users.noreply.github.com>
2021-04-21 12:21:01 +0530

    Add files via upload

---
+++ Introduction to recursion/power_logarithmic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int powerLogarithmic(int x,int n){
+    // write your code here
+    if(n==0)
+        return 1;
+    if(n%2==0){
+        return powerLogarithmic(x,n/2)*powerLogarithmic(x,n/2);
+    }else
+        return powerLogarithmic(x,n-1)*x;
+}
+
+int main(){
+    int x,n; cin>>x>>n;
+    cout<<powerLogarithmic(x,n);
+}
